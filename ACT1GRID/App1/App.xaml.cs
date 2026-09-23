e2c07eb23a4a5dbfa40a55f602c2d6700bdc153d
Maël Dayani Poty <mael.dayani.poty@gmail.com>
2026-09-23 11:12:57 +0200

    grid 1

---
+++ ACT1GRID/App1/App.xaml.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace App1;
+
+public partial class App : Application
+{
+}
